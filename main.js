d63e1c0cd61820bcdde0ad14721800ec7a54b075
Jennifer Parra <jparra.biblio@gmail.com>
2019-04-02 13:28:37 -0300

    estructura lista con visualización en una sola interfaz

--- main.js
+++ main.js
@@ -2,18 +2,42 @@ window.onload = () =>{
 document.getElementById('btnSkills').addEventListener('click',()=>{
     document.getElementById('skills').style.display='block';
     document.getElementById('home').style.display='none';
+    document.getElementById('contact').style.display='none';
+    document.getElementById('works').style.display='none';
 });
 
 document.getElementById('btnHome').addEventListener('click', ()=>{
     document.getElementById('skills').style.display='none';
     document.getElementById('home').style.display='block';
     document.getElementById('works').style.display='none';
-})
+    document.getElementById('contact').style.display='none';
+});
 
 document.getElementById('btnWorks').addEventListener('click',()=>{
-    document.getElementById('works').style.display='block';
+    document.getElementById('portfolio').style.display='block';
     document.getElementById('home').style.display='none';
     document.getElementById('skills').style.display='none';
+    document.getElementById('contact').style.display='none';
+});
+
+document.getElementById('btnContact').addEventListener('click',()=>{
+    document.getElementById('portfolio').style.display='none';
+    document.getElementById('home').style.display='none';
+    document.getElementById('skills').style.display='none';
+    document.getElementById('contact').style.display='block';
 });
 }
+
+$(function(){
+	$('.galeria .contenedor-imagen').on('click', function(){
+		$('#modal').modal;
+		var ruta_imagen = ($(this).find('img').attr('src'));
+		$('#imagen-modal').attr('src', ruta_imagen);
+	});
+
+	$('#modal').on('click', function(){
+		$('#modal').modal('hide');
+	});
+})
+
 
